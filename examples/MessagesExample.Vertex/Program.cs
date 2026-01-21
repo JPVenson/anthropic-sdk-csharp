51bb210c3154fb8371dd208c52dc59ff6ce4a4d0
JPVenson <github@jpb.email>
2026-01-21 14:11:48 +0000

    Add vertex provider Add vertex example
    Add explicit body check for vertex
    
    Readd explicit check for model value in bedrock client
    
    lint
    
    reintroduce null forgiving operator as required by build

---
+++ examples/MessagesExample.Vertex/Program.cs
@@ -0,0 +1,41 @@
+﻿using Anthropic.Models.Messages;
+using Anthropic.Vertex;
+using Google.Apis.Auth.OAuth2;
+
+// The google vertex client needs a Project ID, use the ID from the google cloud dashboard.
+// The region parameter is optional.
+
+// By default the Vertex Credential provider tries to load system wide credentials generated via the "gcloud" tool.
+// For application wide credentials we recommend using service accounts instead and providing your own GoogleCredentials. Example:
+/*
+var client = new AnthropicVertexClient(new AnthropicVertexCredentials(null, "YourProjectId", GoogleCredential.FromJson(
+"""
+{
+    ServiceAccount JSON
+}
+""").CreateScoped("https://www.googleapis.com/auth/cloud-platform")));
+*/
+
+var client = new AnthropicVertexClient(new AnthropicVertexCredentials(null, "YourProjectId"));
+
+MessageCreateParams parameters = new()
+{
+    MaxTokens = 2048,
+    Messages =
+    [
+        new() { Content = "Tell me a story about building the best SDK!", Role = Role.User },
+    ],
+    Model = "claude-sonnet-4-5",
+};
+
+var response = await client.Messages.Create(parameters);
+
+var message = string.Join(
+    "",
+    response
+        .Content.Where(message => message.Value is TextBlock)
+        .Select(message => message.Value as TextBlock)
+        .Select((textBlock) => textBlock.Text)
+);
+
+Console.WriteLine(message);
---
+++ src/Anthropic.Vertex/IAnthropicVertexCredentials.cs
@@ -0,0 +1,24 @@
+namespace Anthropic.Vertex;
+
+/// <summary>
+/// Defines methods for authenticating requests to the vertex api.
+/// </summary>
+public interface IAnthropicVertexCredentials
+{
+    /// <summary>
+    /// Gets the Region on the Project.
+    /// </summary>
+    string Region { get; }
+
+    /// <summary>
+    /// Gets the Project name.
+    /// </summary>
+    string Project { get; }
+
+    /// <summary>
+    /// Applies the authentication method to the request.
+    /// </summary>
+    /// <param name="requestMessage">The http Request message object.</param>
+    /// <returns>A value task that is resolved when the authentication has been applied to the request message.</returns>
+    ValueTask ApplyAsync(HttpRequestMessage requestMessage);
+}
